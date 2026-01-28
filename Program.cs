8f073a4008f2518a274b223eb273a96ec89f1ead
Kryazev <Kryazev@google.com>
2026-01-28 20:40:37 +0300

    Добавление группы и ФИО

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Текущая дата: " + DateTime.Now.ToString("dd.MM.yyyy"));
+        Console.WriteLine("Группа IS22B");
+        Console.WriteLine("ФИО: Slivin");
+        Console.WriteLine("Дата: " + DateTime.Now.ToString("dd.MM.yyyy"));
     }
 }
